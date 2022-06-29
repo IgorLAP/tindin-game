51214c77529a6389572b5a08afe68b224685aacd
Igor Pedrosa <ilap1036@gmail.com>
2022-06-28 21:12:21 -0300

    feat: home component main workflow

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/app/interfaces/Game';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  imagesBanner: { name: string, url: string }[] = [
+    { name: 'placeholder0', url: 'assets/banner0.png' },
+    { name: 'placeholder1', url: 'assets/banner1.png' },
+    { name: 'placeholder2', url: 'assets/banner2.png' },
+  ]
+  actualSlide = 0;
+  widthBanner = `${this.imagesBanner.length * 100}vw`
+  gamesList!: Game[]
+
+  constructor(private gameService: GameService) { }
 
   ngOnInit(): void {
+    this.gameService.listGames()
+      .subscribe({
+        next: (value) => {
+          this.gamesList = value.map(game => {
+            return {
+              ...game,
+              rating: Number(game.rating?.toFixed(1)),
+              mediumPrice: game.mediumPrice
+                ? new Intl.NumberFormat('en-US', { currency: 'USD', style: 'currency' })
+                  .format(game.mediumPrice as number)
+                : 'Price unavailable'
+            }
+          })
+        },
+      })
+  }
+
+  goLeft() {
+    if (this.actualSlide === 0) {
+      this.actualSlide = this.imagesBanner.length - 1
+    } else {
+      this.actualSlide -= 1
+    }
+  }
+
+  goRight() {
+    if (this.actualSlide === this.imagesBanner.length - 1) {
+      this.actualSlide = 0
+    } else {
+      this.actualSlide += 1
+    }
   }
 
 }
