0c3bb5c76a00ca8d5016ae8317a8c41f258cd632
Igor Pedrosa <ilap1036@gmail.com>
2022-06-29 10:28:07 -0300

    feat: signin frontend layout

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  signInForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get email() {
+    return this.signInForm.get('email')
+  }
+
+  get password() {
+    return this.signInForm.get('password')
+  }
+
+  signIn() {
+    console.log('enviou')
   }
 
 }
