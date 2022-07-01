f3d0d94f7a5b9612c58f4455040cb44663b0315e
Igor Pedrosa <ilap1036@gmail.com>
2022-07-01 17:50:03 -0300

    refactor: genre and platform refactoring and exporting file

---
+++ src/app/interfaces/Genre.ts
@@ -0,0 +1,1 @@
+export type Genre = 'Fight' | 'Sports' | 'Survival' | 'Horror' | 'RPG' | 'Fps' | 'Tps' | 'Platform' | 'Adventure' | 'Action' | 'Minigame' | 'Racing' | 'Strategy' | 'Musical' | 'Dance' | 'Simulator';
